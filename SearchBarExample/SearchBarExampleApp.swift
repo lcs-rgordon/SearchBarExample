0224714a5898608b75a94220cec637441472fabe
Russell Gordon <github@russellgordon.ca>
2022-03-25 11:59:55 -0400

    Initial Commit

---
+++ SearchBarExample/SearchBarExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarExampleApp.swift
+//  SearchBarExample
+//
+//  Created by Russell Gordon on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
