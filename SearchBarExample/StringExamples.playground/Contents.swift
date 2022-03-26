168005e0bdb316523aea5b6aafb21dd67f03bd47
Russell Gordon <github@russellgordon.ca>
2022-03-26 08:24:13 -0400

    Added String examples in a playground.

---
+++ SearchBarExample/StringExamples.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+// Does a string contain another string?
+greeting.contains("e")
+greeting.contains("z")
+greeting.contains("h")
+greeting.contains("H")
+
+// Convert a string to lowercase
+greeting.lowercased()
